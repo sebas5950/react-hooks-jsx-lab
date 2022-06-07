9cdfe6d6f5d180cfe10271d9d9acb829f03bd2e2
Alex Guanipatin <sebas5950@gmail.com>
2022-06-07 13:48:10 -0400

    commit tree 5ed9c62911ec336bdbf2a597f737eeae9205e77e

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  //const info = "I honestly love this, but it does get frustrating at times."
+  return (
+  <div id="about"> 
+    <h2>About me</h2>
+    <p>Love learn test so great</p>
+    <img src={image} alt='I made this'></img>
+  </div>
+  )
 }
 
 export default About;
